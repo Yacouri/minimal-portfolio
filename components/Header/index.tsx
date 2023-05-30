ea5d08cdf17b468befd099192cad4a2e682764a2
Yacouri <zouhiryac1@gmail.com>
2023-05-30 17:49:08 +0100

    rework navbar

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,11 +3,19 @@ import React, { useState } from "react";
 
 const Header = () => {
   const [open, setOpen] = useState<Boolean>(false);
+  const [activeLink, setActiveLink] = useState<String>("Home");
+  const routes = [
+    { name: "Home", path: "/" },
+    { name: "Stack", path: "/#Stack" },
+    { name: "Blog", path: "/#Blog" },
+    { name: "Contact", path: "mailto:co.yacouri@gmail.com" },
+  ];
+
   let translateY = open ? 0 : -1000;
   // const [translateY, setTranslateY] = useState<Number>(-1000);
   return (
     <nav className="relative py-2 mt-4 rounded-xl">
-      <div className="flex justify-between items-center">
+      <div className="flex items-center justify-between md:justify-start md:gap-[100px]">
         <div>
           <Link href="/">
             <h1 className="text-white font-bold text-4xl">
@@ -16,27 +24,19 @@ const Header = () => {
           </Link>
         </div>
         <div>
-          <ul className="hidden md:flex gap-9">
-            <li>
-              <Link href="/#tech-stack" className="nav-link">
-                Stack
-              </Link>
-            </li>
-            <li>
-              <Link href="/#work" className="nav-link">
-                Work
-              </Link>
-            </li>
-            <li>
-              <Link href="/#blog" className="nav-link">
-                Blog
-              </Link>
-            </li>
-            <li>
-              <a href="mailto:co.yacouri@gmail.com" className="nav-link">
-                Contact
-              </a>
-            </li>
+          <ul className="hidden md:flex md:gap-[60px] gap-9">
+            {routes.map(({ name, path }, index) => (
+              <li key={index} onClick={() => setActiveLink(name)}>
+                <Link
+                  href={path}
+                  className={`nav-link ${
+                    name === activeLink && "nav-link--active"
+                  }`}
+                >
+                  {name}
+                </Link>
+              </li>
+            ))}
           </ul>
           <div
             className={`flex flex-col gap-[5px] cursor-pointer md:hidden transition-all ${
